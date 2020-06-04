925510f3515f6bac812d41a49a43d3a0c0981dfe
Corey Phillips <corey.lyle.phillips@protonmail.com>
2020-06-04 14:23:29 -0400

    Added lightning to Android.

--- postinstall.js
+++ postinstall.js
@@ -2,10 +2,10 @@ var exec = require("child_process").exec;
 var os = require("os");
 
 function postInstallMac() {
-	exec("cd ios && pod install && cd ..");
+	exec("cd ios && pod install && cd .. && cd node_modules/react-native-lightning && yarn install && ./node_modules/.bin/rn-nodeify --install buffer,stream,assert,events,crypto,vm,process --hack");
 }
 function postInstallLinWin() {
-	//exec("");
+	exec("cd node_modules/react-native-lightning && yarn install && ./node_modules/.bin/rn-nodeify --install buffer,stream,assert,events,crypto,vm,process --hack");
 }
 
 if (os.type() === "Linux")
